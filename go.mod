b07e288d8041d0de323912545ce546846629d139
Tim Möhlmann <muhlemmer@gmail.com>
2022-08-22 14:38:26 +0000

    add AuthStorage implementation scaffolding

--- go.mod
+++ go.mod
@@ -3,11 +3,15 @@ module github.com/muhlemmer/oidc-redis
 go 1.18
 
 require (
+	github.com/go-redis/redis/v9 v9.0.0-beta.2
 	github.com/zitadel/oidc v1.7.0
 	google.golang.org/protobuf v1.28.1
+	gopkg.in/square/go-jose.v2 v2.6.0
 )
 
 require (
+	github.com/cespare/xxhash/v2 v2.1.2 // indirect
+	github.com/dgryski/go-rendezvous v0.0.0-20200823014737-9f7001d12a5f // indirect
 	github.com/felixge/httpsnoop v1.0.1 // indirect
 	github.com/golang/protobuf v1.5.0 // indirect
 	github.com/gorilla/handlers v1.5.1 // indirect
@@ -17,10 +21,9 @@ require (
 	github.com/sirupsen/logrus v1.9.0 // indirect
 	github.com/zitadel/logging v0.3.4 // indirect
 	golang.org/x/crypto v0.0.0-20210921155107-089bfa567519 // indirect
-	golang.org/x/net v0.0.0-20211015210444-4f30a5c0130f // indirect
+	golang.org/x/net v0.0.0-20220425223048-2871e0cb64e4 // indirect
 	golang.org/x/oauth2 v0.0.0-20200902213428-5d25da1a8d43 // indirect
 	golang.org/x/sys v0.0.0-20220715151400-c0bba94af5f8 // indirect
 	golang.org/x/text v0.3.7 // indirect
 	google.golang.org/appengine v1.6.6 // indirect
-	gopkg.in/square/go-jose.v2 v2.6.0 // indirect
 )
--- oidcredis.go
+++ oidcredis.go
@@ -1,1 +1,96 @@
 package oidcredis
+
+import (
+	"context"
+	"errors"
+	"time"
+
+	"github.com/go-redis/redis/v9"
+	"github.com/zitadel/oidc/pkg/oidc"
+	"github.com/zitadel/oidc/pkg/op"
+	"gopkg.in/square/go-jose.v2"
+)
+
+type AuthStorage interface {
+	op.AuthStorage
+	Health(ctx context.Context) error
+}
+
+var ErrUnimplemented = errors.New("unimplemented method")
+
+// increments to the Redis DB number
+const (
+	DBAuthRequests = iota
+	DBTokens
+)
+
+type authStorage struct {
+	auth   *redis.Client
+	tokens *redis.Client
+}
+
+// NewAuthStorage connects to a single redis server.
+// It currently consumes 2 databases:
+//
+//	opts.DB + 0: AuthRequests
+//	opts.DB + 1: Tokens
+func NewAuthStorage(opts redis.Options) AuthStorage {
+	authOpts := opts
+	authOpts.TLSConfig = opts.TLSConfig.Clone()
+	authOpts.DB += DBAuthRequests
+
+	tokenOpts := opts
+	tokenOpts.TLSConfig = opts.TLSConfig.Clone()
+	tokenOpts.DB += DBTokens
+
+	return &authStorage{
+		auth:   redis.NewClient(&authOpts),
+		tokens: redis.NewClient(&authOpts),
+	}
+}
+
+func (s *authStorage) Health(ctx context.Context) error {
+	return ErrUnimplemented
+}
+
+func (s *authStorage) CreateAuthRequest(context.Context, *oidc.AuthRequest, string) (op.AuthRequest, error) {
+	return nil, ErrUnimplemented
+}
+func (s *authStorage) AuthRequestByID(context.Context, string) (op.AuthRequest, error) {
+	return nil, ErrUnimplemented
+}
+func (s *authStorage) AuthRequestByCode(context.Context, string) (op.AuthRequest, error) {
+	return nil, ErrUnimplemented
+}
+func (s *authStorage) SaveAuthCode(context.Context, string, string) error {
+	return ErrUnimplemented
+}
+func (s *authStorage) DeleteAuthRequest(context.Context, string) error {
+	return ErrUnimplemented
+}
+
+func (s *authStorage) CreateAccessToken(context.Context, op.TokenRequest) (accessTokenID string, expiration time.Time, err error) {
+	return "", time.Time{}, ErrUnimplemented
+}
+
+func (s *authStorage) CreateAccessAndRefreshTokens(ctx context.Context, request op.TokenRequest, currentRefreshToken string) (accessTokenID string, newRefreshTokenID string, expiration time.Time, err error) {
+	return "", "", time.Time{}, ErrUnimplemented
+}
+
+func (s *authStorage) TokenRequestByRefreshToken(ctx context.Context, refreshTokenID string) (op.RefreshTokenRequest, error) {
+	return nil, ErrUnimplemented
+}
+
+func (s *authStorage) TerminateSession(ctx context.Context, userID string, clientID string) error {
+	return ErrUnimplemented
+}
+
+func (s *authStorage) RevokeToken(ctx context.Context, tokenID string, userID string, clientID string) *oidc.Error {
+	return &oidc.Error{Parent: ErrUnimplemented}
+}
+
+func (s *authStorage) GetSigningKey(context.Context, chan<- jose.SigningKey) {}
+
+func (s *authStorage) GetKeySet(context.Context) (*jose.JSONWebKeySet, error) {
+	return nil, ErrUnimplemented
+}
